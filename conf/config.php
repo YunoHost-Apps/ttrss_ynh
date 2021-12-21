c3b3b89788f2dba0ef51134ed290f38298df5561
ericgaspar <junk.eg@free.fr>
2021-12-21 16:08:20 +0100

    Update config

--- conf/config.php
+++ conf/config.php
@@ -1,23 +1,17 @@
 <?php
 
-
-
-
-
-
-
 	// *******************************************
 	// *** Database configuration (important!) ***
 	// *******************************************
 
-	putenv('TTRSS_DB_TYPE', "mysql"); // or mysql
-	putenv('TTRSS_DB_HOST', "localhost");
-	putenv('TTRSS_DB_USER', "__DB_NAME__");
-	putenv('TTRSS_DB_NAME', "__DB_NAME__");
-	putenv('TTRSS_DB_PASS', "__DB_PWD__");
-	putenv('TTRSS_DB_PORT', '3306'); // usually 5432 for PostgreSQL, 3306 for MySQL
+	putenv('TTRSS_DB_TYPE=mysql'); // or mysql
+	putenv('TTRSS_DB_HOST=localhost');
+	putenv('TTRSS_DB_USER=__DB_NAME__');
+	putenv('TTRSS_DB_NAME=__DB_NAME__');
+	putenv('TTRSS_DB_PASS=__DB_PWD__');
+	putenv('TTRSS_DB_PORT=3306'); // usually 5432 for PostgreSQL, 3306 for MySQL
 
-	putenv('TTRSS_MYSQL_CHARSET', 'UTF8');
+	putenv('TTRSS_MYSQL_CHARSET=UTF8');
 	// Connection charset for MySQL. If you have a legacy database and/or experience
 	// garbage unicode characters with this option, try setting it to a blank string.
 
@@ -25,7 +19,7 @@
 	// *** Basic settings (important!) ***
 	// ***********************************
 
-	putenv('TTRSS_SELF_URL_PATH', '__DOMAIN_PATH__');
+	putenv('TTRSS_SELF_URL_PATH=__DOMAIN_PATH__');
 	// Full URL of your tt-rss installation. This should be set to the
 	// location of tt-rss directory, e.g. http://example.org/tt-rss/
 	// You need to set this option correctly otherwise several features
@@ -38,11 +32,11 @@
 
 	// Please see PLUGINS below to configure various authentication modules.
 
-	putenv('TTRSS_AUTH_AUTO_CREATE', true);
+	putenv('TTRSS_AUTH_AUTO_CREATE=true');
 	// Allow authentication modules to auto-create users in tt-rss internal
 	// database when authenticated successfully.
 
-	putenv('TTRSS_AUTH_AUTO_LOGIN', true);
+	putenv('TTRSS_AUTH_AUTO_LOGIN=true');
 	// Automatically login user on remote or other kind of externally supplied
 	// authentication, otherwise redirect to login form as normal.
 	// If set to true, users won't be able to set application language
@@ -53,16 +47,16 @@
 	// *** Self-registrations by users ***
 	// ***********************************
 
-	putenv('TTRSS_ENABLE_REGISTRATION', false);
+	putenv('TTRSS_ENABLE_REGISTRATION=false');
 	// Allow users to register themselves. Please be aware that allowing
 	// random people to access your tt-rss installation is a security risk
 	// and potentially might lead to data loss or server exploit. Disabled
 	// by default.
 
-	putenv('TTRSS_REG_NOTIFY_ADDRESS', 'user@your.domain.dom');
+	putenv('TTRSS_REG_NOTIFY_ADDRESS=user@your.domain.dom');
 	// Email address to send new user notifications to.
 
-	putenv('TTRSS_REG_MAX_USERS', 10);
+	putenv('TTRSS_REG_MAX_USERS=10');
 	// Maximum amount of users which will be allowed to register on this
 	// system. 0 - no limit.
 
@@ -70,8 +64,6 @@
 	// *** Cookies and login sessions ***
 	// **********************************
 	
-	define('TTRSS_SESSION_COOKIE_LIFETIME', 86400);
+	define('TTRSS_SESSION_COOKIE_LIFETIME=86400');
 	// Default lifetime of a session (e.g. login) cookie. In seconds, 
 	// 0 means cookie will be deleted when browser closes.
-
-
