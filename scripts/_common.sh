49d9b5d3ee496488152b399d325373ea58d7338d
ericgaspar <junk.eg@free.fr>
2021-12-28 11:38:54 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,8 +6,7 @@
 
 YNH_PHP_VERSION="7.4"
 
-pkg_dependencies="postgresql php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-opcache \
-    php${YNH_PHP_VERSION}-fileinfo php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-xml"
+pkg_dependencies="postgresql php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-opcache php${YNH_PHP_VERSION}-fileinfo php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-xml"
 
 #=================================================
 # EXPERIMENTAL HELPERS
