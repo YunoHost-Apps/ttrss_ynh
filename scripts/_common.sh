46abed3d385d9bc2fa7b2a3c2eb9d2269a93cc4f
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-03-11 13:53:38 +0100

    Testing (#149)
    * Auto-update README
    
    * Upgrade to upstream
    
    * Auto-update README
    
    * Bullseye (#141)
    
    * Fix
    
    * Fix
    
    * Auto-update README
    
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>
    
    * Upgrade
    
    * Auto-update README
    
    * Auto-update README
    
    * Upgrade to upstream
    
    * Auto-update README
    
    * PHP
    
    * Auto-update README
    
    * Update manifest.json
    
    * Auto-update README
    
    * Update config
    
    * Fix
    
    * Auto-update README
    
    * update
    
    * Auto-update README
    
    * Version 2 (#148)
    
    * v2
    
    * v2
    
    * Auto-update README
    
    * Fix
    
    * Auto-update README
    
    * fix
    
    ---------
    
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>
    
    * Update tests.toml
    
    * Delete check_process
    
    * Update restore
    
    * Update remove
    
    * Update install
    
    * Update upgrade
    
    * Fix
    
    * Update upgrade
    
    ---------
    
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="8.0"
-
-# dependencies used by the app
-pkg_dependencies="php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-opcache php${YNH_PHP_VERSION}-fileinfo php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-xml"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
