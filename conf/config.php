6b6e0d8ce280e54da9f31ce18a48d9d9178ff17c
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-03-18 13:45:58 +0100

    fix

--- conf/config.php
+++ conf/config.php
@@ -4,12 +4,12 @@
 	// *** Database configuration (important!) ***
 	// *******************************************
 
-	putenv('TTRSS_DB_TYPE=pgsql'); // or mysql
+	putenv('TTRSS_DB_TYPE=pgsql');
 	putenv('TTRSS_DB_HOST=localhost');
 	putenv('TTRSS_DB_USER=__DB_USER__');
 	putenv('TTRSS_DB_NAME=__DB_NAME__');
 	putenv('TTRSS_DB_PASS=__DB_PWD__');
-	putenv('TTRSS_DB_PORT=5432'); // usually 5432 for PostgreSQL, 3306 for MySQL
+	putenv('TTRSS_DB_PORT=5432');
 
 	// ***********************************
 	// *** Basic settings (important!) ***
